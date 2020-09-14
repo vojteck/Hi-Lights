e633622d11420fece204ecc2e8ad2e9c0bcc3e6c
vojteck <abracadabra@yahoo.com>
2020-09-14 17:56:22 +0200

    Updates

---
+++ precache-manifest.f8664ad747db0aa9ba82fa2583a51208.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa5a5ae4a56a9aab14da2a82830e0c1",
+    "url": "/Hi-Lights/index.html"
+  },
+  {
+    "revision": "1fd4f0ba0366b7fe4abc",
+    "url": "/Hi-Lights/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1fd4f0ba0366b7fe4abc",
+    "url": "/Hi-Lights/static/js/2.68f3387a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Hi-Lights/static/js/2.68f3387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef28e5cc05e53bfee567",
+    "url": "/Hi-Lights/static/js/main.6733017f.chunk.js"
+  },
+  {
+    "revision": "85ccc3eb3e571a45b5f4",
+    "url": "/Hi-Lights/static/js/runtime-main.82e918db.js"
+  }
+]);
